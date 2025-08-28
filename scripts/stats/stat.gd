52124f5d677331d1030bfd8a45aa294775c17250
niiK0 <37949374+niiK0@users.noreply.github.com>
2025-08-28 15:24:34 +0100

    added more stats

--- scripts/stats/stat.gd
+++ scripts/stats/stat.gd
@@ -6,11 +6,9 @@ enum StatType{
 	Bounces,
 	MaxBounces,
 	SpikeChance,
+	PadForce,
+	SlingForce,
 }
 
 @export var statType: StatType
-@export var statValue: float :
-	get: return statValue
-	set(val):
-		statValue = val
-		GlobalSignalBus.emit_signal("stat_value_changed", self, val)
+@export var statValue: float
